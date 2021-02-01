916eff237f2e4df4d75abe8e333c28b47b842450
James Lentzsch <jrlentzsch@gmail.com>
2021-02-01 15:25:14 -0800

    Cookie Monster

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,4 +1,13 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Your JavaScript goes here...
-  
-});
+function cookieMonster(name, value) {
+  document.cookie = `${name}=${value}`
+}
+
+window.onload = () => {
+  console.log("success!");
+
+  document.cookie = "monster_name=cookie";
+  document.cookie = "favorite_cookie=snickerdoodle";
+  for (let i = 0; i < 100; i++) {
+    cookieMonster("cookie" + i, i);
+  }
+};
