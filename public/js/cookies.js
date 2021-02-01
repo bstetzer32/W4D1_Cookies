60239fc7b65ed0b410164946316f8b3145a7bfd2
Ben Stetzer <benstetzer@gmail.com>
2021-02-01 18:52:30 -0500

    get Value

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -2,12 +2,31 @@ function cookieMonster(name, value) {
   document.cookie = `${name}=${value}`
 }
 
+function getCookies() {
+  return document.cookie.split(';')
+}
+
+function getCookieValue(name) {
+  let array = getCookies();
+  for (let i = 0; i < array.length; i++) {
+    let subArray = array[i].split('=');
+    if (subArray[0] === name) {
+      return subArray[1];
+    }
+  }
+  return null;
+}
+
+function deleteCookie(name) {
+  
+}
+
 window.onload = () => {
   console.log("success!");
-
+  
   document.cookie = "monster_name=cookie";
   document.cookie = "favorite_cookie=snickerdoodle";
-  for (let i = 0; i < 100; i++) {
-    cookieMonster("cookie" + i, i);
-  }
+  console.log(getCookies());
+  console.log(getCookieValue("monster_name"));
+  console.log(getCookieValue("Liam_Niesen"));
 };
