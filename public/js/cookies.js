b75be11cf500a8904b95ccfca75fbc5724b19dca
James Lentzsch <jrlentzsch@gmail.com>
2021-02-01 16:32:56 -0800

    delete cookie and beggening of open window

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -18,7 +18,21 @@ function getCookieValue(name) {
 }
 
 function deleteCookie(name) {
-  
+  let array = getCookies();
+  for (let i = 0; i < array.length; i++) {
+    let subArray = array[i].split('=');
+    let cookieName = subArray[0];
+    if (subArray[0] === name) {
+      document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`
+      return;
+    }
+  }
+  alert("Cookie not found!");
+}
+
+function randomWindow(windowName) {
+  let randomNumder = Math.floor(Math.random() * 1000);
+  cookieMonster(windowName, randomNumber)
 }
 
 window.onload = () => {
@@ -26,7 +40,11 @@ window.onload = () => {
   
   document.cookie = "monster_name=cookie";
   document.cookie = "favorite_cookie=snickerdoodle";
-  console.log(getCookies());
-  console.log(getCookieValue("monster_name"));
-  console.log(getCookieValue("Liam_Niesen"));
+  // console.log(getCookies());
+  // console.log(getCookieValue("monster_name"));
+  // console.log(getCookieValue("Liam_Niesen"));
+  // console.log(deleteCookie("monster_name"));
+  // console.log(deleteCookie("Liam_Galager"));
+  // console.log(getCookies());
 };
+
